0184472970dc2bd764744f9004bf7ca3cc02aaf1
Mohamed Adel <m.3del95@gmail.com>
2020-11-23 14:16:17 +0200

    Use Generics With Decodable
    - make utilities Function with Alamofire

--- GenericNetwork/ViewController.swift
+++ GenericNetwork/ViewController.swift
@@ -11,18 +11,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchUserData { (response) in
-            switch response{
-            case .success(let users):
-                guard let users = users else { return }
-                for user in users {
-                    print("id -> \(user.id), name -> \(user.name), email -> \(user.email), username -> \(user.userName), company name -> \(user.company?.name)")
-                }
-            case.failure(_):
-                print("Error")
-            }
-        }
-        fetchPostData { (response) in
+//        fetchUserData { (response) in
+//            switch response{
+//            case .success(let users):
+//                guard let users = users else { return }
+//                for user in users {
+//                    print("id -> \(user.id), name -> \(user.name), email -> \(user.email), username -> \(user.userName), company name -> \(user.company?.name)")
+//                }
+//            case.failure(_):
+//                print("Error")
+//            }
+//        }
+//        fetchPostData { (response) in
+//            switch response{
+//            case .success(let posts):
+//                guard let posts = posts else { return }
+//                for post in posts {
+//                    print("id -> \(post.id), title -> \(post.title), body -> \(post.body)")
+//                }
+//            case.failure(_):
+//                print("Error")
+//            }
+//        }
+        
+        fetchData(url: "https://jsonplaceholder.typicode.com/posts", responseClass: [PostModel].self) { (response) in
             switch response{
             case .success(let posts):
                 guard let posts = posts else { return }
@@ -35,31 +47,41 @@ class ViewController: UIViewController {
         }
     }
     
-
-    func fetchUserData(completion:@escaping (Result<[UserModel]?, NSError>) -> Void) {
-        let url = "https://jsonplaceholder.typicode.com/users"
+    func fetchData<T: Decodable>(url: String, responseClass: T.Type , completion:@escaping (Result<T?, NSError>) -> Void ) {
         AF.request(url, method: .get, parameters: [:], headers: [:]).responseJSON { (response) in
             guard let statuscode = response.response?.statusCode else { return }
             if statuscode == 200 {
                 guard let jsonResponse = try? response.result.get() else { return }
                 guard let theJsonData = try? JSONSerialization.data(withJSONObject: jsonResponse, options: []) else { return }
-                guard let responseObj = try? JSONDecoder().decode([UserModel].self, from: theJsonData) else { return }
+                guard let responseObj = try? JSONDecoder().decode(T.self, from: theJsonData) else { return }
                 completion(.success(responseObj))
             }
         }
     }
-    func fetchPostData(completion:@escaping (Result<[PostModel]?, NSError>) -> Void) {
-        let url = "https://jsonplaceholder.typicode.com/posts"
-        AF.request(url, method: .get, parameters: [:], headers: [:]).responseJSON { (response) in
-            guard let statuscode = response.response?.statusCode else { return }
-            if statuscode == 200 {
-                guard let jsonResponse = try? response.result.get() else { return }
-                guard let theJsonData = try? JSONSerialization.data(withJSONObject: jsonResponse, options: []) else { return }
-                guard let responseObj = try? JSONDecoder().decode([PostModel].self, from: theJsonData) else { return }
-                completion(.success(responseObj))
-            }
-        }
-    }
+//    func fetchUserData(completion:@escaping (Result<[UserModel]?, NSError>) -> Void) {
+//        let url = "https://jsonplaceholder.typicode.com/users"
+//        AF.request(url, method: .get, parameters: [:], headers: [:]).responseJSON { (response) in
+//            guard let statuscode = response.response?.statusCode else { return }
+//            if statuscode == 200 {
+//                guard let jsonResponse = try? response.result.get() else { return }
+//                guard let theJsonData = try? JSONSerialization.data(withJSONObject: jsonResponse, options: []) else { return }
+//                guard let responseObj = try? JSONDecoder().decode([UserModel].self, from: theJsonData) else { return }
+//                completion(.success(responseObj))
+//            }
+//        }
+//    }
+//    func fetchPostData(completion:@escaping (Result<[PostModel]?, NSError>) -> Void) {
+//        let url = "https://jsonplaceholder.typicode.com/posts"
+//        AF.request(url, method: .get, parameters: [:], headers: [:]).responseJSON { (response) in
+//            guard let statuscode = response.response?.statusCode else { return }
+//            if statuscode == 200 {
+//                guard let jsonResponse = try? response.result.get() else { return }
+//                guard let theJsonData = try? JSONSerialization.data(withJSONObject: jsonResponse, options: []) else { return }
+//                guard let responseObj = try? JSONDecoder().decode([PostModel].self, from: theJsonData) else { return }
+//                completion(.success(responseObj))
+//            }
+//        }
+//    }
 }
 
 class UserModel: Decodable {
